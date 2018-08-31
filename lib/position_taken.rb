4e55125b0692776ff17fbc6ef5f8203e717a8ae1
Saint-Jean Germila <germila@noukod.com>
2018-08-31 19:37:09 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,6 @@
 def position_taken?(board, index)
 
-if board[index] == " " "or" board[index] == "" "or"  board[index] == nil
+if board[index] == " " || board[index] == "" ||  board[index] == nil
 
       false
   else
